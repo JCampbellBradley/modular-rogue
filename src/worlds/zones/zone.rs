3eab24e2f4d028d0b7244fa0ea293775248c1887
Campbell Bradley <jcampbellbradley@gmail.com>
2026-09-22 15:50:58 -0600

    Clean up error handling in zone; allow for zero-size zones

--- src/worlds/zones/zone.rs
+++ src/worlds/zones/zone.rs
@@ -1,6 +1,4 @@
-use std::error::Error;
-
-use crate::{entities::entity::EntityId, errors::illegal_argument_error::IllegalArgumentError, worlds::{position::Position, registry::RegistryID, zones::{error_messages::{OUT_OF_BOUNDS, ZERO_SIZE_ZONE}, quadtree::QuadTree}}};
+use crate::{entities::entity::EntityId, errors::illegal_argument_error::IllegalArgumentError, worlds::{position::Position, registry::RegistryID, zones::{error_messages::{OUT_OF_BOUNDS}, quadtree::QuadTree}}};
 
 pub type ZoneID = RegistryID;
 
@@ -12,8 +10,6 @@ pub struct Zone {
 
 impl Zone {
     pub fn new(width: u32, height: u32) -> Self {
-        Self::check_size_valid(width, height);
-
         Self {
             width,
             height,
@@ -25,48 +21,30 @@ impl Zone {
         self.entity_tree.get_all()
     }
 
-    pub fn get_entities(&self, pos: Position) -> Result<impl Iterator<Item = EntityId>, Box<dyn Error>> {
+    pub fn get_entities(&self, pos: Position) -> Result<impl Iterator<Item = EntityId>, IllegalArgumentError<Position>> {
         self.check_position_in_range(pos)?;
         Ok(self.entity_tree.get(pos))
     }
 
-    pub fn put_entity(&mut self, pos: Position, entity: EntityId) -> Result<(), Box<dyn Error>> {
+    pub fn put_entity(&mut self, pos: Position, entity: EntityId) -> Result<(), IllegalArgumentError<Position>> {
         self.check_position_in_range(pos)?;
         self.entity_tree.put(pos, entity);
         Ok(())
     }
 
-    pub fn remove_entity(&mut self, pos: Position, entity: EntityId) -> Result<(), Box<dyn Error>> {
+    pub fn remove_entity(&mut self, pos: Position, entity: EntityId) -> Result<(), IllegalArgumentError<Position>> {
         self.check_position_in_range(pos)?;
         self.entity_tree.remove(pos, entity);
         Ok(())
     }
 
-    fn check_size_valid(width: u32, height: u32) {
-        if width == 0 {
-            let err = IllegalArgumentError {
-                argument_name: "width",
-                argument: width, 
-                reason: ZERO_SIZE_ZONE
-            };
-            panic!("{err}");
-        } else if height == 0 {
-            let err = IllegalArgumentError {
-                argument_name: "height",
-                argument: height, 
-                reason: ZERO_SIZE_ZONE
-            };
-            panic!("{err}");
-        }
-    }
-
-    fn check_position_in_range(&self, pos: Position) -> Result<(), Box<dyn Error>> {
+    fn check_position_in_range(&self, pos: Position) -> Result<(), IllegalArgumentError<Position>> {
         if pos.x >= self.width || pos.y >= self.height {
-            Err(Box::new(IllegalArgumentError {
+            Err(IllegalArgumentError {
                 argument_name: "pos",
                 argument: pos, 
                 reason: OUT_OF_BOUNDS
-            }))
+            })
         }  else {
             Ok(())
         }
@@ -80,9 +58,9 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
     fn zero_size_zone() {
-        let _ = Zone::new(1, 0);
+        let zone = Zone::new(0, 0);
+        let _ = zone.get_all_entities();
     }
 
     #[test]
